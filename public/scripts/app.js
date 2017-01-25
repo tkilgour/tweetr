5b63e3430eac7f2f69070061dbc0ff45a8eec556
Thomas Kilgour <t.kilgour@gmail.com>
2017-01-24 20:20:22 -0500

    implemented tweets and their hover state

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $('.tweet').hover(function() {
+    $(".tweet .hover-icons").css("visibility", "visible");
+    $(".tweet").css("border-color", "#888");
+    $(".tweet .avatar").css("filter", "saturate(100%)")
+  }, function() {
+    $(".tweet .hover-icons").css("visibility", "hidden");
+    $(".tweet").css("border-color", "#ddd");
+    $(".tweet .avatar").css("filter", "saturate(70%)");
+  });
+});
